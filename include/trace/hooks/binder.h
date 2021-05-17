BayLibre/android_kernel_common

ANDROID: binder: Add vendor hooks
Add below vendor hooks to make vendor extensions possible.
	1. android_vh_binder_wakeup_poll_threads_ilocked
	2. android_vh_binder_wakeup_thread_ilocked
	3. android_vh_binder_thread_read

Bug: 174219217
Change-Id: I9f55ca4427583bbfb29a6808280a7d09e1fc22ee
Signed-off-by: Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>

include/trace/hooks/binder.h
@@ -23,10 +23,22 @@ DECLARE_HOOK(android_vh_binder_set_priority,
 DECLARE_HOOK(android_vh_binder_restore_priority,
 	TP_PROTO(struct binder_transaction *t, struct task_struct *task),
 	TP_ARGS(t, task));
+DECLARE_HOOK(android_vh_binder_wakeup_poll_threads_ilocked,
+	TP_PROTO(struct binder_thread *thread, struct task_struct *current),
+	TP_ARGS(thread, current);
+DECLARE_HOOK(android_vh_binder_wakeup_thread_ilocked,
+	TP_PROTO(struct binder_thread *thread, struct task_struct *current),
+	TP_ARGS(thread, current);
+DECLARE_HOOK(android_vh_binder_thread_read,
+	TP_PROTO(struct task_struct *current),
+	TP_ARGS(current);
 #else
 #define trace_android_vh_binder_transaction_init(t)
 #define trace_android_vh_binder_set_priority(t, task)
 #define trace_android_vh_binder_restore_priority(t, task)
+#define trace_android_vh_binder_wakeup_poll_threads_ilocked(thread, current);
+#define trace_android_vh_binder_wakeup_thread_ilocked(thread, current);
+#define trace_android_vh_binder_thread_read(current)
 #endif
 #endif /* _TRACE_HOOK_BINDER_H */
 /* This part must be outside protection */

belowzeroiq/msm_common_kernel

Signed-off-by: Matthias Maennich <maennich@google.com>

include/trace/hooks/binder.h
@@ -22,6 +22,12 @@ DECLARE_HOOK(android_vh_binder_set_priority,
 DECLARE_HOOK(android_vh_binder_restore_priority,
 	TP_PROTO(struct binder_transaction *t, struct task_struct *task),
 	TP_ARGS(t, task));
+DECLARE_HOOK(android_vh_binder_wakeup_poll_threads_ilocked,
+	TP_PROTO(struct task_struct *task),
+	TP_ARGS(task));
+DECLARE_HOOK(android_vh_binder_wakeup_thread_ilocked,
+	TP_PROTO(struct task_struct *task),
+	TP_ARGS(task));
 #endif /* _TRACE_HOOK_BINDER_H */
 /* This part must be outside protection */
 #include <trace/define_trace.h>